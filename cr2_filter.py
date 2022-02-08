ffd5396b3ef4750c4aa5d73d0a93628caf97715d
mikheys <mikheys@gmail.com>
2022-02-08 04:00:07 +0300

    Test

--- cr2_filter.py
+++ cr2_filter.py
@@ -9,36 +9,28 @@ from colorama import Fore, Back, Style
 init()
 
 try:
-    # Вводим пути
-    path_jpg = "null"
+    # Вводим пути и проверяем их наличие
+    path_jpg, path_raw, new_path = "null", "null", "null"
     while os.path.exists(path_jpg) != True:
         path_jpg = input("Введите путь каталога с jpg: ")
-    path_raw = "null"
     while os.path.exists(path_raw) != True:
         path_raw = input("Введите путь каталога с raw: ")
-    new_path = "null"
     while os.path.exists(new_path) != True:
         new_path = input("Введите путь к папке куда будем копировать : ")
 
     # Создаём список из имён файлов jpg и убираем у них расширения
-    file_list_jpg = os.listdir(path_jpg)
-    new_list_jpg = [item[:-4] for item in file_list_jpg]
+    list_jpg = [item[:-4] for item in os.listdir(path_jpg)]
 
     # Создаём счётчики файлов
-    coped_cr2 = 0
-    coped_xmp = 0
+    coped_cr2, coped_xmp = 0, 0
 
     # Цикл перебора файлов в списке и копирование (добавлен прогресс бар tqdm)
-    for i in tqdm(new_list_jpg):
-        path_plus_file_cr2 = f"{path_raw}\{i}.cr2"
-        path_plus_file_xmp = f"{path_raw}\{i}.xmp"
-        path_final_file_cr2 = f"{new_path}\{i}.cr2"
-        path_final_file_xmp = f"{new_path}\{i}.xmp"
-        if os.path.isfile(path_plus_file_cr2):
-            shutil.copy(path_plus_file_cr2, path_final_file_cr2)
+    for i in tqdm(list_jpg):
+        if os.path.isfile(f"{path_raw}\{i}.cr2"):
+            shutil.copy(f"{path_raw}\{i}.cr2", f"{new_path}\{i}.cr2")
             coped_cr2 += 1
-        if os.path.isfile(path_plus_file_xmp):
-            shutil.copy(path_plus_file_xmp, path_final_file_xmp)
+        if os.path.isfile(f"{path_raw}\{i}.xmp"):
+            shutil.copy(f"{path_raw}\{i}.xmp", f"{new_path}\{i}.xmp")
             coped_xmp += 1
 
     # Выводим информацию о количестве скопированных файлов
